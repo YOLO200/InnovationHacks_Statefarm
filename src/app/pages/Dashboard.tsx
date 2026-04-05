0a4a56767ce42d291f03794f9f27af3837e59f87
Yashvi <yashvi8jasani@gmail.com>
2026-04-05 00:59:31 -0700

    dashboard changes

--- src/app/pages/Dashboard.tsx
+++ src/app/pages/Dashboard.tsx
@@ -1,9 +1,6 @@
 import { useAppData } from '../store/AppContext';
 import { mockGigWorker } from '../data/mockData';
 import { FinancialHealthScore } from '../components/dashboard/FinancialHealthScore';
-import { CashRunway } from '../components/dashboard/CashRunway';
-import { IncomeStabilityCard } from '../components/dashboard/IncomeStabilityCard';
-import { SafeBudgetIndicator } from '../components/dashboard/SafeBudgetIndicator';
 import { AIInsights } from '../components/dashboard/AIInsights';
 
 export function Dashboard() {
@@ -13,13 +10,6 @@ export function Dashboard() {
   return (
     <div className="max-w-7xl mx-auto space-y-6">
       <FinancialHealthScore data={data} />
-
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        <CashRunway data={data} />
-        <IncomeStabilityCard data={data} />
-      </div>
-
-      <SafeBudgetIndicator data={data} />
 
       <AIInsights data={data} />
     </div>
